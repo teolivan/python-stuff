6662e3ba7e2b012ce9dfd39ccadd4f7e50fa6fb4
olivia svensson <oliviateoliva@gmail.com>
2022-10-24 20:12:57 +0200

    creating a guessing game and paper rock scissors game

--- main.py
+++ main.py
@@ -13,4 +13,20 @@ def guess(x):
 
     print(f"yay congrats, you have guessed the number {random_number} correctly!!!!")
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback  = input(f"is {guess} too high (H), too low, or correct (C)?? ").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+    print(f"yay your computer guessed your number, {guess}, correctly!")
+
 guess(10)
